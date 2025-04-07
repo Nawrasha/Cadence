c2a9706442318c14053abf2bd07a9d54fb8dea18
unknown <haddadnawras5@gmail.com>
2025-04-07 22:52:41 +0100

    Fusionner nawras + zayneb dans nawras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
-import { Part3Component } from './part3/part3.component';  // Correct the import path
 
 export const routes: Routes = [
-  {
-    path: '', // Default path
-    component: Part3Component
-  }
 ];
 
 
