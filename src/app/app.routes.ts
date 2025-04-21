5788093571d54c7a6f757701edaaa0d08d0de878
zouaouizayneb <zouaouizayneb138@gmail.com>
2025-04-21 19:27:11 +0100

    add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,19 +8,20 @@ import { ListArticleComponent } from './article/list-article/list-article.compon
 import { ProfilComponent } from './profil/profil.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { CreateaccComponent } from './profil/createacc/createacc.component';
-import { AccounteComponent } from './pages/accounte/accounte.component';
+import { AccounteComponent } from './pages/accounte/accounte.component'; 
 import { WishlistComponent } from './pages/wishlist/wishlist.component';
+
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'faq', component: FaqComponent },
   { path: 'articlepage', component: ArticlepageComponent },
-  { path: 'collections', component: CollectionsComponent},
-  { path: 'list-article/:category', component: ListArticleComponent},
+  { path: 'collections', component: CollectionsComponent },
+  { path: 'list-article/:category', component: ListArticleComponent },
   { path: 'profile', component: ProfilComponent },
-  { path: 'contact' , component: ContactComponent},
-  { path: 'createacc', component: CreateaccComponent},
-  { path: 'account', component: AccounteComponent},
-  { path: 'wishlist' , component: WishlistComponent}
+  { path: 'contact', component: ContactComponent },
+  { path: 'createacc', component: CreateaccComponent },
+  { path: 'account', component: AccounteComponent },
+  { path: 'wishlist', component: WishlistComponent }
 ];
