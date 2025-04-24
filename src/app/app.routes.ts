488325db6e9c2e2685cb1f832a977637354292f7
unknown <haddadnawras5@gmail.com>
2025-04-24 21:23:50 +0100

    merge nawras+zayneb

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { FaqComponent } from './pages/faq/faq.component';
 import { ArticlepageComponent } from './pages/articlepage/articlepage.component';
 import { CollectionsComponent } from './pages/collections/collections.component';
 import { ListArticleComponent } from './article/list-article/list-article.component';
+import { ProfilComponent } from './profil/profil.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CreateaccComponent } from './profil/createacc/createacc.component';
+import { AccounteComponent } from './pages/accounte/accounte.component';
+import { WishlistComponent } from './pages/wishlist/wishlist.component';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,4 +18,9 @@ export const appRoutes: Routes = [
   { path: 'articlepage', component: ArticlepageComponent },
   { path: 'collections', component: CollectionsComponent},
   { path: 'list-article/:category', component: ListArticleComponent},
+  { path: 'profile', component: ProfilComponent },
+  { path: 'contact' , component: ContactComponent},
+  { path: 'createacc', component: CreateaccComponent},
+  { path: 'account', component: AccounteComponent},
+  { path: 'wishlist' , component: WishlistComponent}
 ];
